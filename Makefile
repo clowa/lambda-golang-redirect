04aec5b983696dc5e8e21c5f50e36ad8c84b2f49
Gregory Langlais <general@gregorylanglais.com>
2019-01-11 10:17:52 -0500

    add sam local development dependency

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@
 build:
 	dep ensure -v
 	env GOOS=linux go build -ldflags="-s -w" -o bin/redirect redirect/main.go
+	sls sam export -o template.yml
 
 clean:
 	rm -rf ./bin ./vendor Gopkg.lock
--- redirect/main.go
+++ redirect/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -17,23 +15,12 @@ type Response events.APIGatewayProxyResponse
 
 // Handler is our lambda handler invoked by the `lambda.Start` function call
 func Handler(ctx context.Context) (Response, error) {
-	var buf bytes.Buffer
-
-	body, err := json.Marshal(map[string]interface{}{
-		"message": "Okay so your other function also executed successfully!",
-	})
-	if err != nil {
-		return Response{StatusCode: 404}, err
-	}
-	json.HTMLEscape(&buf, body)
-
 	resp := Response{
-		StatusCode:      200,
+		StatusCode:      301,
 		IsBase64Encoded: false,
-		Body:            buf.String(),
+		Body:            "",
 		Headers: map[string]string{
-			"Content-Type":           "application/json",
-			"X-MyCompany-Func-Reply": "redirect",
+			"Location": "http://example.com",
 		},
 	}
 
